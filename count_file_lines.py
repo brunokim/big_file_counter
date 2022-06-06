995111b4518eb76bd7d8af101ba7390a980e63fb
Bruno Kim <brunokim.mc@gmail.com>
2022-06-06 01:10:21 -0300

    Organize count_line_files in a class

--- count_file_lines.py
+++ count_file_lines.py
@@ -1,31 +1,44 @@
 from collections import Counter
+from copy import copy
 from human_size import parse_size, format_size
 import io
 import sys
 from time import time
 
 
-def count_lines(f: io.TextIOBase, memory_limit: int = 1 << 20, check_interval: int = 1024) -> Counter[str]:
-    cnt: Counter[str] = Counter()
-    time_of_last_report: float = 0.0
-    for i, line in enumerate(f):
-        cnt[line[:-1]] += 1
-        if i % check_interval != 0:
-            continue
-        cnt_size = sys.getsizeof(cnt)
-        content_size = sum(sys.getsizeof(key) for key in cnt)
+class LineCounter:
+    def __init__(self, memory_limit: int = 1 << 20, check_interval: int = 1024):
+        self.cnt: Counter[str] = Counter()
+        self.last_time: float = 0.0
+
+        self.memory_limit = memory_limit
+        self.check_interval = check_interval
+
+    def read(self, f: io.TextIOBase) -> Counter[str]:
+        self.cnt.clear()
+        self.last_time = 0.0
+        for i, line in enumerate(f):
+            self.cnt[line[:-1]] += 1
+            if i % self.check_interval != 0:
+                continue
+            self.check_counter_size()
+        return copy(self.cnt)
+
+    def check_counter_size(self) -> None:
+        cnt_size = sys.getsizeof(self.cnt)
+        content_size = sum(sys.getsizeof(key) for key in self.cnt)
         now = time()
-        if now > time_of_last_report + 1:
+        if now > self.last_time + 1:
             print(f'counter size: {format_size(cnt_size)}, content size: {format_size(content_size)}', file=sys.stderr)
-            time_of_last_report = now
-        if cnt_size + content_size > memory_limit:
+            self.last_time = now
+        if cnt_size + content_size > self.memory_limit:
             raise MemoryError('Memory limit exceeded')
-    return cnt
 
 
 def main(filename: str, memory_limit: int) -> None:
+    line_counter = LineCounter(memory_limit=memory_limit)
     with open(filename) as f:
-        cnt = count_lines(f, memory_limit)
+        cnt = line_counter.read(f)
     for elem, freq in cnt.most_common():
         print(f'{elem} - {freq}')
 
